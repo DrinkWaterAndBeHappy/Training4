e619401f76044eb1aac790c87131f8afc26f08c7
DrinkWaterAndBeHappy <s4mur41.95@mail.ru>
2025-12-16 16:33:05 +0300

    Зависимость через конструктор, equals и hashcode

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -2,7 +2,6 @@ package hiber.dao;
 
 import hiber.model.User;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.TypedQuery;
@@ -11,27 +10,30 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    private final SessionFactory sessionFactory;
 
-   @Override
-   public User getUserByCar(String model, int series) {
-      return (User) sessionFactory.getCurrentSession()
-              .createQuery("SELECT u FROM User u WHERE u.car.model = :model and u.car.series = :series")
-              .setParameter("model", model)
-              .setParameter("series", series)
-              .uniqueResult();
-   }
+    public UserDaoImp(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public User getUserByCar(String model, int series) {
+        return (User) sessionFactory.getCurrentSession()
+                .createQuery("SELECT u FROM User u WHERE u.car.model = :model and u.car.series = :series")
+                .setParameter("model", model)
+                .setParameter("series", series)
+                .uniqueResult();
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
 }
